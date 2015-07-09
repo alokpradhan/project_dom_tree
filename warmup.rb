89318eb769cde5f977d4badcad312a610737c920
alokp <alokpra@gmail.com>
2015-07-09 14:55:13 -0700

    working on regex

--- warmup.rb
+++ warmup.rb
@@ -1,6 +1,31 @@
 def parse_tag(tag)
 
-  regex = /<(.*?)>/
-  tags = tag.scan(regex)
-
+  regex = /<(.*?)>/					#matches entrire string within <>
+  whole_line = tag.scan(regex)
+  html = Hash.new
+  #words = whole_line.each.scan(/\w+.*?/)
+  tag = []
+  whole_line.each do |section|
+  	tag << (/\w+.*?/).match(section)
+  end
+  p whole_line
+  p words
 end
+
+
+=begin
+1. Get the whole string
+2. Split tags
+
+Regex needed
+1. capture tags
+2. capture words after outside quotes
+3. capture words between quotes
+4. capture words between > <
+
+Regex tried:
+
+1. /\w+.*?/   matches all words in a string
+
+
+=end
